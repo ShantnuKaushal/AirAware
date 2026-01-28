881e77fc83ba9e00bda7b092160330bc77a8c396
ShantnuKaushal <shntnkaushal@gmail.com>
2026-01-28 15:21:12 -0500

    Easy Start

--- src/AirAware.Weather/Program.cs
+++ src/AirAware.Weather/Program.cs
@@ -1,16 +1,13 @@
 using AirAware.Weather.Services;
 using AirAware.Weather.Grpc;
-using Microsoft.AspNetCore.Server.Kestrel.Core; 
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(options =>
 {
-    // Port 5119: Keep using HTTP 1.1 for Swagger/Browser
-    options.ListenLocalhost(5119, o => o.Protocols = HttpProtocols.Http1);
-
-    // Port 5222: NEW Port specifically for gRPC (HTTP 2)
-    options.ListenLocalhost(5222, o => o.Protocols = HttpProtocols.Http2);
+    options.ListenAnyIP(5119, o => o.Protocols = HttpProtocols.Http1);
+    options.ListenAnyIP(5222, o => o.Protocols = HttpProtocols.Http2);
 });
 
 builder.Services.AddEndpointsApiExplorer();
@@ -29,7 +26,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 app.MapControllers();
 app.MapGrpcService<GrpcWeatherService>();
 
